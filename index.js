2a15e1b92ad1396baa1f44dba0adecba064a72b7
TrujiDev <eetgrisales@gmail.com>
2023-11-26 18:52:49 -0500

    Add express.urlencoded middleware and reorder app.use statements

--- index.js
+++ index.js
@@ -11,8 +11,10 @@ app.use((req, res, next) => {
 	next();
 });
 
-app.use('/', router);
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+
+app.use('/', router);
 
 app.set('view engine', 'pug');
 
