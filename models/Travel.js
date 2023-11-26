e51ceee04369d429fc133dc61d80e43156c1415d
TrujiDev <eetgrisales@gmail.com>
2023-11-25 23:19:53 -0500

    Add Travel model definition

---
+++ models/Travel.js
@@ -0,0 +1,29 @@
+import Sequelize from 'sequelize';
+import db from '../config/db.js';
+
+export const Travel = db.define('travel', {
+	title: {
+		type: Sequelize.STRING,
+	},
+	price: {
+		type: Sequelize.STRING,
+	},
+	departure_date: {
+		type: Sequelize.DATE,
+	},
+	return_date: {
+		type: Sequelize.DATE,
+	},
+	img: {
+		type: Sequelize.STRING,
+	},
+	description: {
+		type: Sequelize.STRING,
+	},
+	availables: {
+		type: Sequelize.STRING,
+	},
+	slug: {
+		type: Sequelize.STRING,
+	},
+});
