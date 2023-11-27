4ebcf375f0592e5f0ab145a02af29301a130cc86
TrujiDev <eetgrisales@gmail.com>
2023-11-27 09:42:16 -0500

    Update database configuration in db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,14 @@ import Sequelize from 'sequelize';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const db = new Sequelize(process.env.DB_URL, {
+const db = new Sequelize(
+	process.env.DATABASE,
+	process.env.DB_USER,
+	process.env.DB_PASSWORD,
+	{
+		host: process.env.DB_HOST,
+		port: process.env.DB_PORT,
+		dialect: 'mysql',
 		define: {
 			timestamps: false,
 		},
