b61a5ccff60fd6a1561a70eb00464f0337a75c11
TrujiDev <eetgrisales@gmail.com>
2023-11-26 21:55:55 -0500

    Refactor database configuration

--- config/db.js
+++ config/db.js
@@ -2,17 +2,25 @@ import Sequelize from 'sequelize';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const db = new Sequelize(process.env.DATABASE_URL, {
-	define: {
-		timestamps: false,
-	},
-	pool: {
-		max: 5,
-		min: 0,
-		acquire: 30000,
-		idle: 10000,
-	},
-	operatorAliases: false,
-});
+const db = new Sequelize(
+	process.env.DB_NAME,
+	process.env.USER,
+	process.env.PASSWORD,
+	{
+		host: process.env.HOST,
+		dialect: 'mysql',
+		port: process.env.PORT,
+		define: {
+			timestamps: false,
+		},
+		pool: {
+			max: 5,
+			min: 0,
+			acquire: 30000,
+			idle: 10000,
+		},
+		operatorAliases: false,
+	}
+);
 
 export default db;
